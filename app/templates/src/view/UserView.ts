2b4d67b0f8d1d2a0395e069fcc314a770d5d8973
Bence Eros <ebence88@gmail.com>
2015-10-19 21:50:02 +0200

    adding require.js to tsd, using it in UserView

--- app/templates/src/view/UserView.ts
+++ app/templates/src/view/UserView.ts
@@ -4,43 +4,38 @@ import Backbone = require("backbone");
 import UserModel = require("../model/UserModel");
 import Handlebars = require("handlebars");
 
-declare var require:(moduleId:string) => any;
 var template = require("text!../template/UserView.html");
 
 class UserView extends Backbone.View<UserModel> {
 
-	protected template: (params?: any) => string;
+    protected template: (params?: any) => string;
 
-	initialize(options: any = {}) {
-		this.template = Handlebars.compile(template);
-		options.model.on("change", this.updateFullName.bind(this));
-	}
-	
-	events() {
-		return {
-			"keyup [name=firstName],[name=lastName]" : "nameTextChanged"
-		};
-	}
-	
-	nameTextChanged() {
-		this.model.set({
-			firstName : this.$("[name=firstName]").val(),
-			lastName : this.$("[name=lastName]").val()
-		});
-	}
-	
-	updateFullName() {
-		this.$("#userName").text(this.model.fullName);
-	}
-	
-	render() {
-		this.$el.html(this.template({
-			firstName : this.model.firstName,
-			lastName : this.model.lastName,
-			fullName : this.model.fullName
-		}));
-		return this;
-	}
+    initialize(options: any = {}) {
+        this.template = Handlebars.compile(template);
+        this.listenTo(this.model, "change", this.updateFullName.bind(this));
+    }
+
+    events() {
+        return {
+            "keyup [name=firstName],[name=lastName]": "nameTextChanged"
+        };
+    }
+
+    nameTextChanged() {
+        this.model.set({
+            firstName: this.$("[name=firstName]").val(),
+            lastName: this.$("[name=lastName]").val()
+        });
+    }
+
+    updateFullName() {
+        this.$("#userName").text(this.model.fullName);
+    }
+
+    render() {
+        this.$el.html(this.template(this.model));
+        return this;
+    }
 }
 
 export = UserView;
