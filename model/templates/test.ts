b6dd74e037e48f25954294480c63402b5c0b0baf
Bence Eros <ebence88@gmail.com>
2015-10-19 22:27:17 +0200

    added template file to create a model with a unittest

---
+++ model/templates/test.ts
@@ -0,0 +1,9 @@
+import <%= sutClassName %> = require("../../model/<%= sutClassName %>.ts);
+
+describe("<%= sutClassName %> Test", function() {
+
+	it("should do something", function() {
+		// TODO
+	});
+
+});
