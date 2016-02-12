0b58a31565810dd440f48bde459ce42fdae3e3c1
Bence Eros <ebence88@gmail.com>
2016-02-12 13:00:44 +0100

    naming cleanup

--- app/templates/src/main/app.ts
+++ app/templates/src/main/app.ts
@@ -1,6 +1,6 @@
 import $ = require("jquery");
-import UserView = require("view/UserView");
-import UserModel = require("model/UserModel");
+import UserView = require("./view/UserView");
+import UserModel = require("./model/UserModel");
 
 function app() {
 	$("body").append(new UserView({model : new UserModel()}).render().$el);
